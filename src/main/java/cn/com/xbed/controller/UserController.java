8b28189a1d4c83576d3a62a0d4d517e35660a28b
BlueSkyyy <leetomscut@163.com>
2017-03-06 13:13:11 +0800

    修改项目架构

--- src/main/java/cn/com/xbed/controller/UserController.java
+++ src/main/java/cn/com/xbed/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 @RestController
 @RequestMapping("test")
-public class HelloController {
+public class UserController {
 
     @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
